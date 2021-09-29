09af032f97fa404eb6f4781869ceae841c7e7729
xMintTea <87257148+xMintTea@users.noreply.github.com>
2021-09-29 19:23:07 +0800

    Added new features

--- helpfull/file_sorter/sorter.py
+++ helpfull/file_sorter/sorter.py
@@ -1,15 +1,17 @@
-from genericpath import isfile
 import tkinter, os, shutil, tkinter.filedialog, tkinter.messagebox
 
 def file_type_da(file_name):
-    file_type = file_name[::-1]
-    dote = file_type.index('.')
-    file_type = file_type[:dote]
-    file_type = file_type[::-1]
-    if " " in file_type:
-        index = file_type.index(" ")
-        file_type = file_type[:index]
-    return file_type
+    if "." not in file_name:
+        pass
+    else:
+        file_type = file_name[::-1]
+        dote = file_type.index('.')
+        file_type = file_type[:dote]
+        file_type = file_type[::-1]
+        if " " in file_type:
+            index = file_type.index(" ")
+            file_type = file_type[:index]
+        return file_type
 
 
 username = os.environ.get( "USERNAME" )
@@ -32,28 +34,40 @@ for i in files:
         file_type = file_type_da(i)
     except ValueError:
         continue
-    NewPath = sort_folder+"\\"+file_type.upper()
+    try:
+        NewPath = sort_folder+"\\"+file_type.upper()
+    
+        if os.path.exists(NewPath) == False:
+            os.mkdir(NewPath, dir_fd=None)
 
-    if os.path.exists(NewPath) == False:
-        os.mkdir(NewPath, dir_fd=None)
+        kk = file_type in i
 
-    kk = file_type in i
+        if kk == True:
+            shutil.move(sort_folder+"\\"+i,NewPath)
+        else:
+            continue
 
-    if kk == True:
-        shutil.move(sort_folder+"\\"+i,NewPath)
-    else:
+        for ii in files:
+            isFile = os.path.isdir(sort_folder+"\\"+ii)
+
+            if isFile == True:
+                if len(os.listdir(sort_folder+"\\"+ii)) == 0 and not sort_files_in_folders:
+                    os.rmdir(sort_folder+"\\"+ii, dir_fd=None)
+    except:
         continue
 
-for ii in files:
-    isFile = os.path.isdir(sort_folder+"\\"+ii)
 
-    if isFile == True:
-        if len(os.listdir(sort_folder+"\\"+ii)) == 0 and not sort_files_in_folders:
-            os.rmdir(sort_folder+"\\"+ii, dir_fd=None)
 
-# files_in_sort_files_in_folders = os.listdir(path=sort_files_in_folders)
+for i in files:
+    try:
+        check_empty_folder = (os.listdir(path=sort_folder+"\\"+i))
+        if os.path.isdir(sort_folder+"\\"+i) == True and len(check_empty_folder) == 0:
+            os.rmdir(sort_folder+"\\"+i, dir_fd=None)
+    except NotADirectoryError:
+        if os.path.exists == False:
+            os.mkdir(sort_folder+"\\"+"FILES")
+            shutil.move(sort_folder+"\\"+i,sort_folder+"\\"+"FILES")
+        else:
+            shutil.move(sort_folder+"\\"+i,sort_folder+"\\"+"FILES")
+        
 
-# for i in files_in_sort_files_in_folders:
-#     if os.path.isdir(sort_files_in_folders+"\\"+i) == True and len((sort_files_in_folders+"\\"+i)) != 0:
-#         for u in os.listdir(path=sort_files_in_folders):
-#             print(u)
